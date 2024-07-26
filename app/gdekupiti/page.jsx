ec0b4268658171be1d1d030f4679c021a8b59c5b
MacBook <>
2024-07-26 12:50:30 +0200

    gde kupiti - links

--- app/gdekupiti/page.jsx
+++ app/gdekupiti/page.jsx
@@ -17,51 +17,73 @@ const Gdekupiti = () => {
         <div className="md:w-[80%] mx-auto p-2">
         <h3 className="font-semibold text-[22px]"> Gde kupiti Tied up proizvode?</h3>
         <div className="grid grid-cols-4 gap-6 mt-[4rem]">
-            
+          <Link href="https://maps.app.goo.gl/aVR5BCJ5DpfKeB4D7" target="_blank" >
             <div className="col-span-1">
-              <Image src={store1} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Katapult Handmade</h5>
-              <p className="text-center font-light">Knez Mihailova 36, Beograd</p>
+              <div className="overflow-hidden rounded-[20px]">
+              <Image src={store1} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Katapult Handmade</h5>
+              <p className="text-center font-light hover:underline">Knez Mihailova 36, Beograd</p>
             </div>
+            </Link>
+            <Link href="https://maps.app.goo.gl/PExADP5DPLKoQtke6" target="_blank">
+              <div className="col-span-1">
+              <div className="overflow-hidden rounded-[20px]">
+                <Image src={store2} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+                <h5 className="text-center mt-5 text-[20px] hover:underline">TRICIKL Art Craft Gift Shop</h5>
+                <p className="text-center font-light hover:underline">Francuska 5, Beograd</p>
+              </div>
+            </Link>
+            <Link href="https://maps.app.goo.gl/kBshMaBHcoPQVfeR6" target="_blank">
+              <div className="col-span-1">
+              <div className="overflow-hidden rounded-[20px]">
+                <Image src={store3} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+                <h5 className="text-center mt-5 text-[20px] hover:underline">Caruso</h5>
+                <p className="text-center font-light hover:underline">27. marta 19, TC Radnički, Kragujevac</p>
+              </div>
+            </Link>
+            <Link href="https://maps.app.goo.gl/LEx73s32BtDdGNpS8" target="_blank">
             <div className="col-span-1">
-              <Image src={store2} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">TRICIKL Art Craft Gift Shop</h5>
-              <p className="text-center font-light">Francuska 5, Beograd</p>
-            </div>
-            <div className="col-span-1">
-              <Image src={store3} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Caruso</h5>
-              <p className="text-center font-light">27. marta 19, TC Radnički, Kragujevac</p>
-            </div>
-            <div className="col-span-1">
-              <Image src={store4} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Džentlmen</h5>
-              <p className="text-center font-light">Vuka Karadžić 1b, 
+            <div className="overflow-hidden rounded-[20px]">
+              <Image src={store4} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Džentlmen</h5>
+              <p className="text-center font-light hover:underline">Vuka Karadžić 1b, 
 Gornji Milanovac</p>
             </div>
+            </Link>
         </div>
         <div className="grid grid-cols-4 gap-6 mt-[4rem]">
-            
+          <Link href="https://maps.app.goo.gl/SHTapNJKVLEp4NBP6" target="_blank">
             <div className="col-span-1">
-              <Image src={store5} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Bleu Homme</h5>
-              <p className="text-center font-light">Vice Vukova 6, ARENA Center, Zagreb</p>
+            <div className="overflow-hidden rounded-[20px]">
+              <Image src={store5} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Bleu Homme</h5>
+              <p className="text-center font-light hover:underline">Vice Vukova 6, ARENA Center, Zagreb</p>
             </div>
+          </Link>
+          <Link href="https://maps.app.goo.gl/Z9LReXfDYmessmfm9" target="_blank">
             <div className="col-span-1">
-              <Image src={store6} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Bleu Homme</h5>
-              <p className="text-center font-light">Akcije Maslenica 1, SUPERNOVA, Zadar</p>
+            <div className="overflow-hidden rounded-[20px]">
+              <Image src={store6} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Bleu Homme</h5>
+              <p className="text-center font-light hover:underline">Akcije Maslenica 1, SUPERNOVA, Zadar</p>
             </div>
+          </Link>
+          <Link href="https://maps.app.goo.gl/meWHn7UFYjqWcV4j8" target="_blank">
             <div className="col-span-1">
-              <Image src={store7} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Bleu Homme</h5>
-              <p className="text-center font-light">Vukovarska 16, MERCANTE Center, Dubrovnik</p>
+            <div className="overflow-hidden rounded-[20px]">
+              <Image src={store7} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Bleu Homme</h5>
+              <p className="text-center font-light hover:underline">Vukovarska 16, MERCANTE Center, Dubrovnik</p>
             </div>
+          </Link>
+          <Link href="https://maps.app.goo.gl/7inupH1SDYh7whtK7" target="_blank">
             <div className="col-span-1">
-              <Image src={store8} alt="" width={400} height={400} className="rounded-[20px]"/>
-              <h5 className="text-center mt-5 text-[20px]">Bleu Homme</h5>
-              <p className="text-center font-light">Josipa Jovića 93, Mall of Split, Split</p>
+            <div className="overflow-hidden rounded-[20px]">
+              <Image src={store8} alt="" width={400} height={400} className="rounded-[20px] w-full transition-all duration-500 hover:!scale-110"/></div>
+              <h5 className="text-center mt-5 text-[20px] hover:underline">Bleu Homme</h5>
+              <p className="text-center font-light hover:underline">Josipa Jovića 93, Mall of Split, Split</p>
             </div>
+          </Link>
         </div>
         </div>
       </div>
